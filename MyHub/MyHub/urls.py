aa8479d0d316be3c62d05735327d863d2772ffdc
sbarbier <sbarbier@S3bBookAir.local>
2013-12-26 16:20:55 +0100

    Add Bootstrap and settings config on staticFiles

--- MyHub/MyHub/urls.py
+++ MyHub/MyHub/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from MyHub.home.views import home_page
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,4 +12,4 @@ urlpatterns = patterns('',
 
     url(r'^$', home_page, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
