7299abda4c105483206ffcd26b2ddd565a98fbe8
Sébastien BARBIER <contact@sebastienbarbier.com>
2015-06-14 18:09:20 +0200

    LESS to CSS with minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-var gulp = require('gulp');
-
-var less = require('gulp-less');
-var path = require('path');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css'),
+    watch = require('gulp-watch');
 
 gulp.task('less', function () {
   return gulp.src('./sebastienbarbier/static/css/styles.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
+    .pipe(less())
+  	.pipe(minifyCSS())
     .pipe(gulp.dest('./sebastienbarbier/static/css/'));
 });
+
+gulp.task('serve', function () {
+    watch('./sebastienbarbier/static/css/**/*.less', function () {
+        gulp.start('less');
+    });
+});
+
+gulp.task('default', ['serve']);
