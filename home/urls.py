e42becdb987ecf87b416ec6e2e4423155f8ef656
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-09-23 14:39:51 +0700

    Add legal page and populate index page with content

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'^legal$', views.legal, name='legal'),
 ]
