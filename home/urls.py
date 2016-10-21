04e6994bfca6d04a8f2eac225a38c1da27822b8b
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-10-21 13:29:58 +0700

    First draft with new design

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^contact$', views.contact, name='contact'),
     url(r'^legal$', views.legal, name='legal'),
 ]
