75109a2db5032a73b23b0c33a7d71303917fc3b2
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-09-07 14:38:38 +0700

    Delete Django site now deprecated

--- djangosite/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^about$', views.about, name='about'),
-    url(r'^contact$', views.contact, name='contact'),
-    url(r'^legal$', views.legal, name='legal'),
-]
