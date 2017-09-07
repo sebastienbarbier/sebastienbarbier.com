299323f159917253cc34c0d673843947b4948e53
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-09-07 13:40:22 +0700

    Projet now running on angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+import { ContactComponent } from './contact/contact.component';
+import { LegalComponent } from './legal/legal.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: 'legal',      component: LegalComponent }
+  // { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent,
+    LegalComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
