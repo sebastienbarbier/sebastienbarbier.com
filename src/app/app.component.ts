140be0a50fabb322c526f74e11df913da95564a2
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-09-08 12:13:53 +0700

    Refactor Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
