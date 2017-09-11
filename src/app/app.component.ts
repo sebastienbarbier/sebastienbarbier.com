34336ffece62e55ef20f417896695ce56f0abeb6
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-09-11 20:53:48 +0700

    Add dummy animation on pageChangeEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,30 @@ import 'rxjs/add/operator/mergeMap';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routingState', [
+      transition('* => *', [
+        style({transform: 'translateY(20px)', opacity: 0}),
+        animate(200, style({transform: 'translateY(0)', opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
-  theme: string;
+  routerState: string;
   title = 'app';
 
   constructor(
@@ -21,21 +37,15 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-
-    // From : https://toddmotto.com/dynamic-page-titles-angular-2-router-events
-    this.router.events
-      .filter((event) => event instanceof NavigationEnd)
-      .map(() => this.route)
-      .map((route) => {
-        while (route.firstChild) {  route = route.firstChild; }
-        return route;
-      })
-      .filter((route) => route.outlet === 'primary')
-      .mergeMap((route) => route.data)
-      .subscribe((event) =>
-        setTimeout(() => {
-          this.theme = event['theme'];
-        }, 10));
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.routerState = event.url;
+      }
+      // NavigationStart
+      // NavigationEnd
+      // NavigationCancel
+      // NavigationError
+      // RoutesRecognized
+    });
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -24,6 +25,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserAnimationsModule,
     RouterModule.forRoot(
       appRoutes
     )
