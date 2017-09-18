bd0b09e0c24d7ec00f4181661c1644ebc5891d82
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-09-18 12:45:47 +0700

    Change transition default animation from slide to fade

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -17,19 +17,19 @@ import {
 export const routerTransition = trigger('routerTransition', [
   transition('* => *', [
     query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {optional: true}),
-    query(':enter', style({ transform: 'translateX(100%)' }), {optional: true}),
+    query(':enter', style({ opacity: 0 }), {optional: true}),
     sequence([
       query(':leave', animateChild(), {optional: true}),
       group([
         query(':leave', [
-          style({ transform: 'translateX(0%)' }),
+          style({ opacity: 1 }),
           animate('500ms cubic-bezier(.75,-0.48,.26,1.52)',
-          style({ transform: 'translateX(-100%)' }))
+          style({ opacity: 0 }))
         ], {optional: true}),
         query(':enter', [
-          style({ transform: 'translateX(100%)' }),
+          style({ opacity: 0 }),
           animate('500ms cubic-bezier(.75,-0.48,.26,1.52)',
-          style({ transform: 'translateX(0%)' }))
+          style({ opacity: 1 }))
         ], {optional: true}),
       ]),
       query(':enter', animateChild(), {optional: true})
