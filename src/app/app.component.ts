dff1a8cc71e352153dd159fbc73bba4feb474b2a
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-10-19 10:29:36 +0700

    Implement dark theme on server rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 
-import { Component, OnInit, Inject, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2, ElementRef, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { routerTransition } from './router.animations';
+
+ import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 import {
   trigger,
@@ -29,12 +31,14 @@ export class AppComponent implements OnInit {
       private route: ActivatedRoute,
       private router: Router,
       private element: ElementRef,
-      private renderer: Renderer2
+      private renderer: Renderer2,
+      @Inject(PLATFORM_ID) private platformId: Object
     ) {
     this.headerState = 'hide';
     this.navigationMenuStatus = false;
     this.element = element;
     this.renderer = renderer;
+    this.platformId = platformId;
   }
 
   ngOnInit() {
@@ -56,7 +60,14 @@ export class AppComponent implements OnInit {
   }
 
   getState(outlet) {
-    this.renderer.setAttribute(this.element.nativeElement.parentElement, 'class', outlet.activatedRouteData.theme);
+   if (isPlatformBrowser(this.platformId)) {
+      // Client only code.
+      this.renderer.setAttribute(this.element.nativeElement.parentElement, 'class', outlet.activatedRouteData.theme);
+   }
+   if (isPlatformServer(this.platformId)) {
+     // Server only code.
+      this.renderer.setAttribute(this.element.nativeElement.parent, 'class', outlet.activatedRouteData.theme);
+   }
     return outlet.activatedRouteData.state;
   }
 
