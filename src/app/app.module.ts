4c270b34b7b124ab9deeddff0f22dde4d6d24e8f
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-10-19 10:17:42 +0700

    Delete dead code (blog part, gonna be writting)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,15 +12,12 @@ import { LegalComponent } from './legal/legal.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { PhotographyComponent } from './photography/photography.component';
-import { BlogComponent } from './blog/blog.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, data: { title: 'Home', theme: 'light', state: 'home' } },
   { path: 'projects', component: ProjectsComponent, data: { title: 'Projects', theme: 'light', state: 'projects' } },
   { path: 'photography', component: PhotographyComponent, data: { title: 'Photography', theme: 'dark', state: 'photography' } },
-  { path: 'blog', component: BlogComponent, data: { title: 'Blog', theme: 'light', state: 'blog' } },
   { path: 'contact', component: ContactComponent, data: { title: 'Contact', theme: 'light', state: 'contact' } },
   { path: 'legal', component: LegalComponent, data: { title: 'Legal & copyright', theme: 'light', state: 'legal' } },
   { path: '**', component: PageNotFoundComponent, data: { title: 'Page not found', theme: 'light', state: '404' }  }
@@ -43,7 +40,6 @@ const appRoutes: Routes = [
     HomeComponent,
     ProjectsComponent,
     PhotographyComponent,
-    BlogComponent,
     PageNotFoundComponent
   ],
   providers: [],
