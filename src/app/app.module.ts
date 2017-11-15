b52e01d2ccb7f69b2ee4e056ea3109aaecc7c9fb
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-11-15 13:50:37 +0700

    Add freelance page with Mila description.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { LegalComponent } from './legal/legal.component';
 import { HomeComponent } from './home/home.component';
+import { FreelanceComponent } from './freelance/freelance.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { PhotographyComponent } from './photography/photography.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -26,11 +27,12 @@ export function createTranslateLoader(http: HttpClient) {
 // State is use to bind animation
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, data: { title: null, theme: 'light', state: 'home' } },
+  { path: 'freelance', component: FreelanceComponent, data: { title: 'nav.freelance', theme: 'light', state: 'freelance' } },
   { path: 'projects', component: ProjectsComponent, data: { title: 'nav.projects', theme: 'light', state: 'projects' } },
   { path: 'photography', component: PhotographyComponent, data: { title: 'nav.photography', theme: 'dark', state: 'photography' } },
   { path: 'contact', component: ContactComponent, data: { title: 'nav.contact', theme: 'light', state: 'contact' } },
   { path: 'legal', component: LegalComponent, data: { title: 'nav.legal', theme: 'light', state: 'legal' } },
-  { path: '**', component: PageNotFoundComponent, data: { title: 'nav.404.title', theme: 'light', state: '404' }  }
+  { path: '**', component: PageNotFoundComponent, data: { title: null, theme: 'light', state: '404' }  }
 ];
 
 @NgModule({
@@ -56,6 +58,7 @@ const appRoutes: Routes = [
     LegalComponent,
     HomeComponent,
     ProjectsComponent,
+    FreelanceComponent,
     PhotographyComponent,
     PageNotFoundComponent
   ],
