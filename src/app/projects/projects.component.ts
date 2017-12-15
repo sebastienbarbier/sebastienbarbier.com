b4b308172303981d55e6fb0180b0a2cf475283b0
Sébastien BARBIER <contact@sebastienbarbier.com>
2017-12-15 11:57:21 +0700

    Add primitive slideshow on sevent23 project thumbnails

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -35,13 +35,38 @@ const projectTransition = trigger('projectTransition', [
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() {
+  selectedPhoto;
+  photos = [
+    { thumbnail: 'https://public.sebastienbarbier.com/images/seven23/screenshots/1-dashboard.png',
+      source: 'https://public.sebastienbarbier.com/images/seven23/screenshots/large/1-dashboard.png',
+      alt: 'Seven23 Dashboard' },
+    { thumbnail: 'https://public.sebastienbarbier.com/images/seven23/screenshots/2-transactions.png',
+      source: 'https://public.sebastienbarbier.com/images/seven23/screenshots/large/2-transactions.png',
+      alt: 'Seven23 Transactions' },
+    { thumbnail: 'https://public.sebastienbarbier.com/images/seven23/screenshots/3-changes.png',
+      source: 'https://public.sebastienbarbier.com/images/seven23/screenshots/large/3-changes.png',
+      alt: 'Seven23 Changes' },
+    { thumbnail: 'https://public.sebastienbarbier.com/images/seven23/screenshots/4-categories.png',
+      source: 'https://public.sebastienbarbier.com/images/seven23/screenshots/large/4-categories.png',
+      alt: 'Seven23 Categories' },
+    { thumbnail: 'https://public.sebastienbarbier.com/images/seven23/screenshots/5-settings.png',
+      source: 'https://public.sebastienbarbier.com/images/seven23/screenshots/large/5-settings.png',
+      alt: 'Seven23 Settings' }
+  ];
 
+  constructor() {
+    this.selectedPhoto = null;
   }
 
   @HostBinding('@projectTransition') '';
 
   ngOnInit() {
+  }
+
+  view(event, photo = null) {
+    this.selectedPhoto = photo;
+    // Disable href if javascript is activated
+    event.preventDefault();
   }
 
 }
