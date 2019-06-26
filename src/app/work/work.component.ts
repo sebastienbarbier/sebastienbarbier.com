5d183b750e77f37a0d47624676dbb0d1ae2c671d
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-06-26 17:19:01 +0800

    Refactor work page layout

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -39,19 +39,38 @@ export class WorkComponent implements OnInit {
   photos = [
     { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-dashboard-desktop-light.png',
       source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-dashboard-desktop-light.png',
-      alt: 'Seven23 Dashboard' },
+      alt: 'Seven23 Dashboard desktop' },
+    { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-dashboard-mobile-light.png',
+      source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-dashboard-mobile-light.png',
+      alt: 'Seven23 Dashboard mobile' },
+
     { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-transactions-desktop-light.png',
       source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-transactions-desktop-light.png',
-      alt: 'Seven23 Transactions' },
+      alt: 'Seven23 Transactions desktop' },
+    { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-transactions-mobile-light.png',
+      source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-transactions-mobile-light.png',
+      alt: 'Seven23 Transactions mobile' },
+
     { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-change-desktop-light.png',
       source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-change-desktop-light.png',
-      alt: 'Seven23 Changes' },
+      alt: 'Seven23 Changes desktop' },
+    { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-change-mobile-light.png',
+      source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-change-mobile-light.png',
+      alt: 'Seven23 Changes mobile' },
+
     { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-categories-desktop-light.png',
       source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-categories-desktop-light.png',
-      alt: 'Seven23 Categories' },
+      alt: 'Seven23 Categories desktop' },
+    { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-categories-mobile-light.png',
+      source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-categories-mobile-light.png',
+      alt: 'Seven23 Categories mobile' },
+
     { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-report-desktop-light.png',
       source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-report-desktop-light.png',
-      alt: 'Seven23 Report' }
+      alt: 'Seven23 Report desktop' },
+    { thumbnail: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/small/01-report-mobile-light.png',
+      source: 'https://cellar-c2.services.clever-cloud.com/cdn.seven23.io/static/images/screenshots/large/01-report-mobile-light.png',
+      alt: 'Seven23 Report mobile' }
   ];
 
   constructor(@Inject(DOCUMENT) private d ) {
