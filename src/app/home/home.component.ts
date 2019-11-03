92ebcedc04d28217b605cab5b00b7e89f03fe3bf
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-11-03 20:19:41 +0700

    Improve homepage, define buttons, and add mapbox on homepage for location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -55,6 +55,7 @@ export class HomeComponent implements OnInit {
     this.hasIntersectionObservable = IntersectionObserver != undefined;
   }
 
+
   @HostBinding('@homeTransition') '';
 
   ngOnInit() {
@@ -70,6 +71,89 @@ export class HomeComponent implements OnInit {
       observer.observe(document.querySelector("#observer1"));
       observer.observe(document.querySelector("#observer2"));
       observer.observe(document.querySelector("#observer3"));
+
+      mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViYXN0aWVuYmFyYmllciIsImEiOiJjanoxdnNqZWMwcGs3M25sb3h2YmdkbDZ3In0.-dJlheQDJc4rl34gH8a0-A';
+      setTimeout(() => {
+        var map = new mapboxgl.Map({
+          container: 'map', // container id
+          style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+          center: [nomadlist.location.now.longitude, nomadlist.location.now.latitude], // starting position [lng, lat]
+          zoom: 2 // starting zoom
+        });
+        map.scrollZoom.disable();
+        map.on('load', function () {
+          var size = 50;
+          const pulsingDot = {
+            width: size,
+            height: size,
+            data: new Uint8Array(size * size * 4),
+
+            onAdd: function() {
+              var canvas = document.createElement('canvas');
+              canvas.width = this.width;
+              canvas.height = this.height;
+              this.context = canvas.getContext('2d');
+            },
+
+            render: function() {
+              var duration = 1000;
+              var t = (performance.now() % duration) / duration;
+
+              var radius = size / 2 * 0.3;
+              var outerRadius = size / 2 * 0.7 * t + radius;
+              var context = this.context;
+
+              // draw outer circle
+              context.clearRect(0, 0, this.width, this.height);
+              context.beginPath();
+              context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+              context.fillStyle = 'rgba(219, 183, 120,' + (1 - t) + ')';
+              context.fill();
+
+              // draw inner circle
+              context.beginPath();
+              context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+              context.fillStyle = 'rgba(219, 183, 120, 1)';
+              context.strokeStyle = 'white';
+              context.lineWidth = 2 + 4 * (1 - t);
+              context.fill();
+              context.stroke();
+
+              // update this image's data with data from the canvas
+              this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+              // keep the map repainting
+              map.triggerRepaint();
+
+              // return `true` to let the map know that the image was updated
+              return true;
+            }
+          };
+
+          map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+          map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+              "type": "geojson",
+              "data": {
+                "type": "FeatureCollection",
+                "features": [{
+                  "type": "Feature",
+                  "geometry": {
+                    "type": "Point",
+                    "coordinates": [nomadlist.location.now.longitude, nomadlist.location.now.latitude]
+                  }
+                }]
+              }
+            },
+            "layout": {
+              "icon-image": "pulsing-dot"
+            }
+          });
+        });
+      }, 600);
+
     }
   }
 
