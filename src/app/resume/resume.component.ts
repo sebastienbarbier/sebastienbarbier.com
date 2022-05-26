377a0be6107fa316f024d476ea2746ad7516372b
Sébastien Barbier <contact@sebastienbarbier.com>
2022-05-26 10:47:19 +0200

    Add flags to experiences locations

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -37,7 +37,7 @@ export class ResumeComponent implements OnInit {
         company: {
           name: 'SAP',
           url: 'sap.com',
-          location: 'Zurich, Switzerland'
+          location: 'Zurich, Switzerland 🇨🇭'
         },
         title: 'Senior Developer and Associate Development Architect',
         description: 'Full stack lead developer (6 month), then Associate Development Architect within the SAP FSM project. Working with micro front-end framework to build a unified interface. Angular base with node backend. ',
@@ -46,7 +46,7 @@ export class ResumeComponent implements OnInit {
         date: 'November 2016 - January 2020 (3 years and 3 months)',
         company: {
           name: 'Self employed',
-          location: 'Remote work, Thailand',
+          location: 'Remote work, Thailand 🇹🇭',
         },
         title: 'Web technologies consultant and freelancer',
         description: 'Senior front-end for companies like Mila, Mozilla, or Chefclub.',
@@ -56,7 +56,7 @@ export class ResumeComponent implements OnInit {
         company: {
           name: 'Fluance',
           url: 'fluance.ch',
-          location: 'Solothurn, Switzerland',
+          location: 'Solothurn, Switzerland 🇨🇭',
         },
         title: 'Front-end Software Engineer',
         description: 'Full-time front-end development on Fluance project : developed an app to aggregate content from HL7 feed of clinics in a generic unified web interface. Team of four front-end. Used mostly AngularJS/jQuery. Manager with Gulp, tested with Jasmine/Karma. Documentation generated using Dgeni. Using SCRUM method.',
@@ -65,7 +65,7 @@ export class ResumeComponent implements OnInit {
         date: 'September 2012 - June 2014 (1 year and 10 months)',
         company: {
           name: 'Covalia',
-          location: 'Besançon, France',
+          location: 'Besançon, France 🇫🇷',
         },
         title: 'Software Engineer',
         description: 'Work on Covotem, a video chat application for remote medical consultation. Mostly worked on : Java with internal libraries. Interoperability in healthcare system (HL7, DICOM).',
@@ -73,9 +73,9 @@ export class ResumeComponent implements OnInit {
       {
         date: 'First semester 2011 and first semester 2012',
         company: {
-          name: 'university of ISIFC and UFR ST',
+          name: 'University of Franche-Comté (ISIFC and UFR ST)',
           url: 'univ-fcomte.fr',
-          location: 'Besançon, France',
+          location: 'Besançon, France 🇫🇷',
         },
         title: 'Lecturer',
         description: 'Java Courses. Practical, Tutorial class, and Projects - respectively 66 and 32 hours, third year students. ',
@@ -84,7 +84,7 @@ export class ResumeComponent implements OnInit {
         date: 'July 2009 - June 2012 (2 years part time, 1 year full time)',
         company: {
           name: 'Ionyse',
-          location: 'Besançon, France',
+          location: 'Besançon, France 🇫🇷',
         },
         title: 'Co-founder and web developer',
         description: 'Start this project in parallel of our studies, to help financing them. After 24 months part-time change to a full-time job. Web agency, worked on web application for small companies. ',
