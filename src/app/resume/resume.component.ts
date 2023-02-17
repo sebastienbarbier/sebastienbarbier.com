544becf025b91db1ef1434a84b37ea7017b24f4c
Sébastien Barbier <contact@sebastienbarbier.com>
2023-02-17 17:27:45 +0100

    First version of the experience part of resume with logo

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -50,61 +50,105 @@ export class ResumeComponent implements OnInit {
 
     this.experiences = [
       {
-        date: 'February 2020 - July 2021 (18 months)',
-        company: {
-          name: 'SAP',
-          url: 'sap.com',
-          location: 'Zurich, Switzerland'
-        },
-        title: 'Senior Developer and Associate Development Architect',
-        description: 'Full stack lead developer (6 month), then Associate Development Architect within the SAP FSM project. Working with micro front-end framework to build a unified interface. Angular base with node backend. ',
+        name: 'SAP',
+        url: 'sap.com',
+        location: 'Zurich, Switzerland',
+        logo: '/assets/images/sap/sap.svg',
+        type: 'Full-time',
+        positions: [{
+          title: 'Associate Development Architect',
+          date: {
+           from: new Date('August 01, 2020'),
+           until: new Date('July 31, 2021'),
+          },
+          description: 'Associate Development Architect within the SAP FSM project. Working with micro front-end framework to build a unified interface. Angular base with node backend. ',
+        }, {
+          title: 'Senior Developer',
+          date: {
+           from: new Date('February 01, 2020'),
+           until: new Date('July 31, 2020'),
+          },
+          description: 'Full stack lead developer within the SAP FSM project. Working with micro front-end framework to build a unified interface. Angular base with node backend. ',
+        }],
       },
       {
-        date: 'November 2016 - January 2020 (3 years and 3 months)',
-        company: {
-          name: 'Self employed',
-          location: 'Remote work, Thailand',
-        },
-        title: 'Web technologies consultant and freelancer',
-        description: 'Senior front-end for companies like Mila, Mozilla, or Chefclub.',
+        name: 'Self employed',
+        location: 'Chiang-Mai, Thailand. Remote work',
+        type: 'Freelance',
+        logo: '/assets/images/sprb/sebastienbarbier_logo.png',
+        positions: [{
+          title: 'Web technologies consultant and freelancer',
+          date: {
+           from: new Date('November 01, 2016'),
+           until: new Date('January 31, 2020'),
+          },
+          description: 'Senior front-end for companies like Mila AG, Mozilla, or Chefclub.',
+        }],
       },
       {
-        date: 'June 2014 - August 2016 (2 years and 3 months)',
-        company: {
-          name: 'Fluance',
-          url: 'fluance.ch',
-          location: 'Solothurn, Switzerland',
-        },
-        title: 'Front-end Software Engineer',
-        description: 'Full-time front-end development on Fluance project : developed an app to aggregate content from HL7 feed of clinics in a generic unified web interface. Team of four front-end. Used mostly AngularJS/jQuery. Manager with Gulp, tested with Jasmine/Karma. Documentation generated using Dgeni. Using SCRUM method.',
+        name: 'Fluance AG',
+        url: 'fluance.ch',
+        location: 'Solothurn, Switzerland',
+        type: 'Full-time',
+        logo: '/assets/images/fluance/fluance_logo.png',
+        positions: [{
+          title: 'Front-end Software Engineer',
+          date: {
+           from: new Date('June 01, 2014'),
+           until: new Date('August 31, 2016'),
+          },
+          description: 'Full-time front-end development on Fluance project : developed an app to aggregate content from HL7 feed of clinics in a generic unified web interface. Team of four front-end. Used mostly AngularJS/jQuery. Manager with Gulp, tested with Jasmine/Karma. Documentation generated using Dgeni. Using SCRUM method.',
+        }],
       },
       {
-        date: 'September 2012 - June 2014 (1 year and 10 months)',
-        company: {
-          name: 'Covalia',
-          location: 'Besançon, France',
-        },
-        title: 'Software Engineer',
-        description: 'Work on Covotem, a video chat application for remote medical consultation. Mostly worked on : Java with internal libraries. Interoperability in healthcare system (HL7, DICOM).',
+        name: 'Covalia',
+        location: 'Besançon, France',
+        type: 'Full-time',
+        logo: '/assets/images/covalia/covalia_logo.png',
+        positions: [{
+          title: 'Software Engineer',
+          date: {
+           from: new Date('September 01, 2012'),
+           until: new Date('June 30, 2014'),
+          },
+          description: 'Work on Covotem, a video chat application for remote medical consultation. Mostly worked on : Java with internal libraries. Interoperability in healthcare system (HL7, DICOM).',
+        }],
       },
       {
-        date: 'First semester 2011 and first semester 2012',
-        company: {
-          name: 'University of Franche-Comté (ISIFC and UFR ST)',
-          url: 'univ-fcomte.fr',
-          location: 'Besançon, France',
-        },
-        title: 'Lecturer',
-        description: 'Java Courses. Practical, Tutorial class, and Projects - respectively 66 and 32 hours, third year students. ',
+        name: 'University of Franche-Comté',
+        url: 'univ-fcomte.fr',
+        location: 'Besançon, France',
+        type: 'Part-time',
+        logo: '/assets/images/univ-fcomte/univ-fcomte_logo.png',
+        positions: [{
+          title: 'Part-time Lecturer at ISIFC and UFR ST',
+          date: {
+           from: new Date('September 01, 2012'),
+           until: new Date('January 31, 2013'),
+          },
+          description: 'Java Courses. Practical, Tutorial class, and Projects - 32 hours, third year students. In parallel to my work at Covalia Interactive S.A.',
+        }, {
+          title: 'Part-time Lecturer at ISIFC and UFR ST',
+          date: {
+           from: new Date('September 01, 2011'),
+           until: new Date('December 31, 2011'),
+          },
+          description: 'Java Courses. Practical, Tutorial class, and Projects - 66 hours, third year students. In parallel to my work at Ionyse.',
+        }],
       },
       {
-        date: 'July 2009 - June 2012 (2 years part time, 1 year full time)',
-        company: {
-          name: 'Ionyse',
-          location: 'Besançon, France',
-        },
-        title: 'Co-founder and Full-stack developer',
-        description: 'Start this project in parallel of our studies, to help financing them. After 24 months part-time change to a full-time job. Web agency, worked on web application for small companies. ',
+        name: 'Ionyse',
+        location: 'Besançon, France',
+        type: 'Full-time then Part-time',
+        logo: '/assets/images/ionyse/ionyse_logo.png',
+        positions: [{
+          title: 'Co-founder and Full-stack developer',
+          date: {
+           from: new Date('July 01, 2009'),
+           until: new Date('June 30, 2012'),
+          },
+          description: 'Start this project in parallel of our studies, to help financing them. After 24 months part-time change to a full-time job. Web agency, worked on web application for small companies. ',
+        }],
       }
     ];
 
@@ -130,7 +174,7 @@ export class ResumeComponent implements OnInit {
         internship: {
           company: 'MagicLabVision Laboratory',
           location: 'Adelaide, Australia',
-          description: 'Joined a research team on an augmented reality application based on Python.',
+          description: 'Research team on an augmented reality application based on Python.',
           duration_in_months: 3,
         }
       }, {
@@ -145,7 +189,7 @@ export class ResumeComponent implements OnInit {
         location: 'Belfort, France',
         internship: {
           company: 'Thales Avionics Electrical Systems',
-          location: 'Chatou, France',
+          location: 'Chatou, Île-de-France, France',
           description: 'PHP Developer, using Oracle databases, access archives from a previous ERP.',
           duration_in_months: 3,
         }
