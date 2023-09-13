a0cf2507a7d8fcbabadd230d55322159b1ebaa06
Sébastien Barbier <contact@sebastienbarbier.com>
2023-09-13 20:46:40 +0200

    Update resume content and styling

--- src/app/resume/resume.data.ts
+++ src/app/resume/resume.data.ts
@@ -11,7 +11,7 @@ export default {
         date: {
          start: new Date('August 01, 2021'),
         },
-        description: `On August 2021, I left my position at SAP to dedicate time studying blockchain technologies and solutions to bootstraps some ideas as projects. I learned a lot about Defi and NFTs, while in the mean time building tools that I would own and wanted to use for myself. Feeling solid technically, my main challenge goes around marketing and being able to provide a great user experience.`,
+        description: `I am now embraced the exciting challenge of **crafting my own products**, commencing with **[Seven23](https://seven23.io)**, a **comprehensive manual budgeting application**, and **[FromEdwin](https://fromedwin.com)**, a sleek **monitoring platform tailored for web projects**. Simultaneously, I continue to engage in **cryptocurrency trading**, maintaining my keen interest in this dynamic market.`,
       }],
     },
     {
@@ -27,22 +27,22 @@ export default {
          start: new Date('November 01, 2020'),
          end: new Date('July 31, 2021'),
         },
-        description: `Associate Development Architect in the Product Engineering - S/4HANA Cloud - Field Service Management (FSM) Self-Service & Platform Extensibility department.
-Responsible for the following tasks:
-- Working in tech lead position, coaching and advising the implementation team on all aspects which have impact on the architecture as well as advocating for simplicity and latest Best Practices
-- Taking on the responsibility for development focusing on design, coding, testing, and quality assurance of complex product features, incl. resolving complex issues within own area of expertise and supporting others in resolving issues
-- Contributing to the aligned and consistent architecture strategy and target architecture of product line
-- Evaluating architecture risks, estimating effort for business case or backlog item, and preparing management decisions on these topics
-- Supporting and consulting Product Owner by ensuring technical and architectural feasibility, readiness, and compliance
-- Contributing to designing and building the FSM extensibility concept, driving product adoption through technical support for partners and customers, meaningful code samples, and public webinars
-- Mentoring Junior Developers`,
+        description: `Leveraging the newly integrated micro-frontend architecture, I contributed **designing and implementing** a **distributed in-app extension solution**, enabling seamless and secure integration for **third-party developers** directly within our web interface.
+My responsabilities also included:
+- **Working in tech lead position**, **coaching** and **advising the implementation team** on all aspects which have impact on the architecture as well as advocating for simplicity and latest best practices
+- Taking on the **responsibility for development** focusing on design, coding, testing, and quality assurance of complex product features, incl. resolving complex issues within own area of expertise and supporting others in resolving issues
+- Contributing to the aligned and **consistent architecture strategy and target architecture** of product line
+- **Evaluating architecture risks**, estimating effort for business case or backlog item, and preparing management decisions on these topics
+- **Supporting and consulting Product Owner** by ensuring technical and architectural feasibility, readiness, and compliance
+- Contributing to **designing and building the FSM extensibility concept**, driving product adoption through technical support for partners and customers, meaningful code samples, and public webinars
+- **Mentoring Junior Developers**`,
       }, {
         title: 'Senior Developer',
         date: {
          start: new Date('February 01, 2020'),
          end: new Date('October 31, 2020'),
         },
-        description: 'Taking on the responsibility for development focusing on design, coding, testing, and quality assurance of complex product features, incl. resolving complex issues within own area of expertise and supporting others in resolving issues.',
+        description: 'I joined the team responsible for spearheading the migration of the [Field Service Management (FSM)](https://www.sap.com/products/scm/field-service-management.html) application to a **micro-frontend architecture**, contributing to the modernization and scalability of the development infrastructure. Taking on the **responsibility for development** focusing on design, coding, testing, and quality assurance of complex product features, incl. resolving complex issues within own area of expertise and supporting others in resolving issues.',
       }],
     },
     {
@@ -57,7 +57,7 @@ Responsible for the following tasks:
          start: new Date('September 01, 2016'),
          end: new Date('January 31, 2020'),
         },
-        description: `I leveraged my full-stack knowledge to support startups and tech companies in building solutions using the latest web technologies. While being great at integrating diverse projects, I particularly enjoyed tackling complex front-end challenges. Notable clients I have worked with include **Chefclub**, **Mozilla**, and **Mila AG**.`,
+        description: `I leveraged my **full-stack knowledge** to support startups and tech companies in building solutions using the latest web technologies. While being great at integrating diverse projects, I particularly enjoyed tackling **complex front-end challenges**. Notable clients I have worked with include **Chefclub**, **Mozilla**, and **Mila AG**.`,
         images: [
           { src: '/assets/images/chefclub/chefclub-logo.png', alt: 'Chefclub logo'},
           { src: '/assets/images/mozilla/mozilla_logo.svg', alt: 'Mozilla logo'},
@@ -78,7 +78,7 @@ Responsible for the following tasks:
          start: new Date('June 01, 2014'),
          end: new Date('August 31, 2016'),
         },
-        description: 'I served as a full-time front-end developer on the Fluance project, leading a team of four in developing an application that aggregates content from HL7 feeds of clinics into a unified web interface. Leveraging my expertise in AngularJS and jQuery, I managed the project with Gulp and tested it using Jasmine/Karma. I prioritized documentation, generating it using Dgeni, and utilized the SCRUM method to ensure project success.',
+        description: 'I served as a **full-time front-end developer** on the Fluance project, **leading a team of four** in developing an application that **aggregates content from HL7 feeds** of clinics into a unified web interface. Leveraging my expertise in **AngularJS** and **jQuery**, I managed the project with **Gulp** and tested it using **Jasmine/Karma**. I prioritized **documentation**, generating it using **Dgeni**, and utilized the **SCRUM method** to ensure project success.',
       }],
     },
     {
@@ -93,7 +93,7 @@ Responsible for the following tasks:
          start: new Date('September 01, 2012'),
          end: new Date('June 30, 2014'),
         },
-        description: `I joined Covalia as a Research Engineer in September 2012 to prepare for a Ph.D. in Cloud Computing within the healthcare environment. After 7 months, I transitioned to the existing development team as a core-developer. My primary expertise was in Java, and I mostly used internal libraries to ensure smooth project operation. My responsibilities centered around ensuring interoperability within the healthcare system, specifically with HL7 and DICOM protocols, in a real-time environment. Additionally, I worked with IP Cameras (Axis) and followed ISO 13485 standard. In addition to my development work, I also provided occasional deployment and phone support to the team.`,
+        description: `I joined Covalia as a **Research Engineer** to prepare for a Ph.D. in **Cloud Computing within the healthcare environment**. After 7 months, I transitioned to the existing development team as a core-developer. My primary expertise was in **Java**, and I mostly used **internal libraries** to ensure smooth project operation. My responsibilities centered around ensuring **interoperability** within the healthcare system, specifically with **HL7 and DICOM protocols**, in a **real-time environment**. Additionally, I worked with **IP Cameras (Axis)** and followed **ISO 13485 standard**. In addition to my development work, I also provided occasional **deployment** and **phone support to customers**.`,
       }],
     },
     {
@@ -109,14 +109,14 @@ Responsible for the following tasks:
          start: new Date('September 01, 2012'),
          end: new Date('January 31, 2013'),
         },
-        description: 'In addition to my role at Covalia Interactive S.A., I provided 32 hours of practical Java and HTML/CSS instructions to third-year students at both ISIFC and UFR ST. This included delivering tutorial classes and project-based learning opportunities.',
+        description: 'In addition to my position at Covalia Interactive S.A., I provided **32 hours of practical Java and HTML/CSS instructions** to third-year students at both **ISIFC** and **UFR ST**. This included delivering tutorial classes and project-based learning opportunities.',
       }, {
         title: 'Part-time Lecturer',
         date: {
          start: new Date('September 01, 2011'),
          end: new Date('January 31, 2012'),
         },
-        description: 'In addition to my role at Ionyse, I provided 66 hours of practical Java instruction to third-year students at both ISIFC and UFR ST. This included delivering tutorial classes and project-based learning opportunities.',
+        description: 'In addition to my position at Ionyse, I provided **66 hours of practical Java instruction to third-year students** at both **ISIFC** and **UFR ST**. This included delivering tutorial classes and project-based learning opportunities.',
       }],
     },
     {
@@ -131,7 +131,7 @@ Responsible for the following tasks:
          start: new Date('July 01, 2009'),
          end: new Date('June 30, 2012'),
         },
-        description: `A web agency was founded by me and two partners while I was pursuing my master's degree, as a student job. After working part-time for two years, the job became a full-time position. Our focus was on creating websites and web applications for small businesses, utilizing the Django framework. Due to the small size of our agency, I had the opportunity to explore a wide range of topics, ultimately specializing in front-end development, with a particular focus on HTML5, CSS3, and jQuery. In addition, we created our own CMS, which was called ionyweb.`,
+        description: `**Founded in parallel of our master degree** a **web agency** with **two other students** which after two years transitioned to a full-time position. Our focus was on creating **websites** and **web applications** for small businesses, utilizing the **Django framework**. Due to the small size of our agency, I had the opportunity to explore a wide range of topics, ultimately specializing in **front-end development**, with a particular focus on **HTML5, CSS3, and jQuery**. In addition, we created our own **CMS**, which was called **ionyweb**.`,
       }],
     }
   ],
