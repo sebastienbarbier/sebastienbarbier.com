2168c330c63831ac32cda8fa7429140f1ff949ce
Sébastien Barbier <contact@sebastienbarbier.com>
2024-06-17 21:51:51 +0200

    Refactor styling to use @sebastienbarbier/design-system and remove Firefox temporary fix
    Reated to #104 #97

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import design from '@sebastienbarbier/design-system';
+
+console.log(design);
+
 if (environment.production) {
   enableProdMode();
 }
